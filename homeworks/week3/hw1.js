dfcb9d64e8099469ad2978ea7fe7159101c29021
ALANYEN0202 <a02852000@gmail.com>
2020-06-22 09:40:22 +0800

    hw1

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,1 +1,13 @@
-
+function repeat(str, n) {
+  let result = '';
+  for (let i = 1; i <= n; i += 1) {
+    result += str;
+  }
+  return result;
+}
+function star(n) {
+  for (let i = 1; i <= n; i += 1) {
+    console.log(repeat('*', i));
+  }
+}
+star(5);
